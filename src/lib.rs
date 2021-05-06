ea0b4b1bb0f24272a07db52dceb59038d6e27d81
Nick Mosher <nicholastmosher@gmail.com>
2021-05-06 17:07:58 -0400

    Add option for map or filter

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
 use fluvio_smartstream::{smartstream, SimpleRecord};
 
+{% if smartstream-type == "filter" %}
 #[smartstream(filter)]
-fn my_filter(_record: &SimpleRecord) -> bool {
-    true
+fn my_filter(record: &SimpleRecord) -> bool {
+    String::from_utf8_lossy(&record.value).contains('a')
 }
+{% else %}
+#[smartstream(map)]
+fn my_filter(mut record: SimpleRecord) -> SimpleRecord {
+    record.value.make_ascii_uppercase();
+    record
+}
+{% endif %}
