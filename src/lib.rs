a5c135bd378f591f9bbccf1af31edf008c5037b8
Nick Mosher <nicholastmosher@gmail.com>
2021-05-19 13:37:36 -0400

    Use string slice instead of String, rename to 'filter'

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,13 @@ use fluvio_smartstream::{smartstream, Record};
 
 {% if smartstream-type == "filter" %}
 #[smartstream(filter)]
-fn my_filter(record: &Record) -> bool {
-    String::from_utf8_lossy(&record.value).contains('a')
+pub fn filter(record: &Record) -> bool {
+    let str_result = std::str::from_utf8(record.value.as_ref());
+    let string = match str_result {
+        Ok(s) => s,
+        _ => return false,
+    };
+
+    string.contains('a')
 }
 {% endif %}
