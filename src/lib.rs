c98dbaa855843cde7f750b47088b58a427768b99
Nick Mosher <nicholastmosher@gmail.com>
2021-05-19 08:43:12 -0400

    Change SimpleRecord to Record

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
-use fluvio_smartstream::{smartstream, SimpleRecord};
+use fluvio_smartstream::{smartstream, Record};
 
 {% if smartstream-type == "filter" %}
 #[smartstream(filter)]
-fn my_filter(record: &SimpleRecord) -> bool {
+fn my_filter(record: &Record) -> bool {
     String::from_utf8_lossy(&record.value).contains('a')
 }
 {% else %}
 #[smartstream(map)]
-fn my_filter(mut record: SimpleRecord) -> SimpleRecord {
+fn my_filter(mut record: Record) -> Record {
     record.value.make_ascii_uppercase();
     record
 }
