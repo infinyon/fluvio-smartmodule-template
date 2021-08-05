a8f1934b47fcc5c9a8eabb552601cc0b6ee8977e
Nick Mosher <nicholastmosher@gmail.com>
2021-08-05 12:24:59 -0400

    Update template for fluvio-smartstream-0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,21 @@
-use fluvio_smartstream::{smartstream, Record};
-
 {% if smartstream-type == "filter" %}
-#[smartstream(filter)]
-pub fn filter(record: &Record) -> bool {
-    let str_result = std::str::from_utf8(record.value.as_ref());
-    let string = match str_result {
-        Ok(s) => s,
-        _ => return false,
-    };
+use fluvio_smartstream::{smartstream, Result, Record};
 
+#[smartstream(filter)]
+pub fn filter(record: &Record) -> Result<bool> {
+    let string = std::str::from_utf8(record.value.as_ref())?;
     string.contains('a')
+}
+{% else if smartstream-type == "map" %}
+use fluvio_smartstream::{smartstream, Result, Record, RecordData};
+#[smartstream(map)]
+pub fn map(record: &Record) -> Result<(Option<RecordData>, RecordData)> {
+    let key = record.key.clone();
+
+    let string = std::str::from_utf8(record.value.as_ref())?;
+    let int = string.parse::<i32>()?;
+    let value = (int * 2).to_string();
+
+    Ok((key, value.into()))
 }
 {% endif %}
