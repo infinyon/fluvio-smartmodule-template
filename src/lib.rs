2985944fc323ce98427cd094593bb8dfb9d6332d
Nick Mosher <nicholastmosher@gmail.com>
2021-08-18 11:07:38 -0400

    Add aggregate template (#5)
    * Add aggregate template
    
    * Use elsif
    
    * Use 0.2 to pull latest when published

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ pub fn filter(record: &Record) -> Result<bool> {
     let string = std::str::from_utf8(record.value.as_ref())?;
     Ok(string.contains('a'))
 }
-{% else if smartstream-type == "map" %}
+{% elsif smartstream-type == "map" %}
 use fluvio_smartstream::{smartstream, Result, Record, RecordData};
 #[smartstream(map)]
 pub fn map(record: &Record) -> Result<(Option<RecordData>, RecordData)> {
@@ -17,5 +17,15 @@ pub fn map(record: &Record) -> Result<(Option<RecordData>, RecordData)> {
     let value = (int * 2).to_string();
 
     Ok((key, value.into()))
+}
+{% elsif smartstream-type == "aggregate" %}
+use fluvio_smartstream::{smartstream, Result, Record, RecordData};
+
+#[smartstream(aggregate)]
+pub fn aggregate(accumulator: RecordData, current: &Record) -> Result<RecordData> {
+    let mut acc = String::from_utf8(accumulator.as_ref().to_vec())?;
+    let next = std::str::from_utf8(current.value.as_ref())?;
+    acc.push_str(next);
+    Ok(acc.into())
 }
 {% endif %}
